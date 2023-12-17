9e26df011de29e12773c143e39d64fbbbbfe8ab1
mif <miftah165cm@gmail.com>
2023-12-18 02:21:42 +0700

    initial commit

---
+++ site.config.js
@@ -0,0 +1,68 @@
+const CONFIG = {
+  // profile setting (required)
+  profile: {
+    name: "Miftah Rizky Aulia",
+    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    role: "fsoftware engineer",
+    bio: "I code your dreams",
+    email: "kBXGr0@proton.me",
+    linkedin: "",
+    github: "ifrzky",
+    instagram: "ifrizy_",
+  },
+  projects: [
+    {
+      name: `Pangoline Engine`,
+      href: "https://github.com/ifrzky/pangoline-engine",
+    },
+  ],
+  // blog setting (required)
+  blog: {
+    title: "miftah",
+    description: "welcome to my blog!",
+  },
+
+  // CONFIG configration (required)
+  link: "https://blog.miftahrizkyaulia.me",
+  since: 2023, // If leave this empty, current year will be used.
+  lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+  ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
+
+  // notion configuration (required)
+  notionConfig: {
+    pageId: process.env.NOTION_PAGE_ID,
+  },
+
+  // plugin configuration (optional)
+  googleAnalytics: {
+    enable: false,
+    config: {
+      measurementId: process.env.NEXT_PUBLIC_GOOGLE_MEASUREMENT_ID || "",
+    },
+  },
+  googleSearchConsole: {
+    enable: false,
+    config: {
+      siteVerification: process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION || "",
+    },
+  },
+  utterances: {
+    enable: false,
+    config: {
+      repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
+      "issue-term": "og:title",
+      label: "💬 Utterances",
+    },
+  },
+  cusdis: {
+    enable: false,
+    config: {
+      host: "https://cusdis.com",
+      appid: "", // Embed Code -> data-app-id value
+    },
+  },
+  isProd: process.env.VERCEL_ENV === "production", // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
+  revalidateTime: 21600 * 7, // revalidate time for [slug], index
+}
+
+module.exports = { CONFIG }
